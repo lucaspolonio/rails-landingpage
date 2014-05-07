cdbb45de8f41da192674f302fb46a7046fcc1ba9
Lucas Polonio <polonio.lucas@gmail.com>
2014-05-07 18:06:19 -0300

    only configures redis for heroku if in production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Split.redis = REDIS
+if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  Split.redis = REDIS
+end
